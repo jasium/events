51c0424863d4bb788a00da1219e6ebae1c5dfa25
John J. Mikucki <john.j.mikucki@gmail.com>
2012-03-21 14:08:34 -0400

    wrote some basic tests for event

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @valid_attributes =   {  name: "Ruby Conf",
+                             description: "A ruby conference that needs at least 25 characters",
+                             location: "Reston, VA",
+                             capacity: 100,
+                             price: 9.99,
+                             starts_at: "2012-03-20 13:42:25",
+                             image_file_name: "image.png"
+                         }
+  end
+
+  test "can create a valid event" do
+    event = Event.new(@valid_attributes)
+    assert event.valid?
+  end
+
+  test "event needs a name" do
+    event = Event.new(@valid_attributes.except(:name))
+    assert event.invalid?, "Event needs a name"
+  end
+
+  test "event is free when price is zero" do
+    event = Event.new(@valid_attributes.merge(price: 0))
+    assert event.free?
+  end
+  test "event not free when price is nonzero" do
+    event = Event.new(@valid_attributes)
+    refute event.free?
+  end
+  test "event is free when price is nil" do
+    event = Event.new(@valid_attributes)
+    refute event.free?
+  end
 end
